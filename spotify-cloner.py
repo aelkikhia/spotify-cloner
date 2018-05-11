8ca66cb973e5866663377fc2e0c03e77d42e1ff7
Taz Elkikhia <aelkikhia@gmail.com>
2018-05-11 02:48:28 -0500

    working on put

--- spotify-cloner.py
+++ spotify-cloner.py
@@ -3,37 +3,36 @@ import json
 import requests
 # from urllib import request, parse
 
-READ_AUTH_TOKEN = ""
-MODIFY_AUTH_TOKEN = ""
+READ_AUTH_TOKEN = "BQA7NaDximJ0RWHIpbmjD5k3ZM_9Bf_YGOJUj0GQIcuCuejH4DsoW7cw6_3tJ0YVA6d5mD0E58kxCk7TnWMWkM1CYtx1ikVtjwoEvWP5_eijYgh1CbdFOzQz7av5WsG_2NRVypJj4DWuTXzJ_gENMxUwM0E0CbMzdwdBVu_OTtOv4kZ-ecufo_Ga5bfSFXaK1QKW6Vb0Ykt0o7lBE-6DSZ3zL6YVVCDf6a9hFVUXMREbVUQdoTtCKUficmVgTElZ3ceONp64vBc"
+MODIFY_AUTH_TOKEN = "BQCwi6qoSgrzq2ug14Qj9a2mCQPgiqFy3fQ7zvE2eFScL7Qwg1wzQpWOMdOyN-6boQl0oSp3J4qpJ0NeWzgiE46guhfVsnyeHH0L8P8n8ZGJiZ3mPT83dsZFNRH9BHnx6iIvzblQkjwdwq8NoeyVpBgeu0zUPI4IIG0_V6f9aCmy-Oi4TDzLrlQBx5W8L7WJbeMzFucQ0iNTElviQyiZNbvaHczVpvrsEBWSNYctK69gQUzEtebRuXs1EORC8IV_EJGa-Q"
 
-SPOTIFY_API_URI = "https://api.spotify.com/v1/"
+ALBUM_URI = "https://api.spotify.com/v1/me/albums"
 GET_HEADERS = {'Authorization': "Bearer %s" % READ_AUTH_TOKEN,
-               "Accept": "application/json"}
-POST_HEADERS = {}
+               "Content-Type": "application/json"}
+PUT_HEADERS = {'Authorization': "Bearer %s" % MODIFY_AUTH_TOKEN,
+               "Content-Type": "application/json"}
 
 
 def run_transfer():
     pass
 
 
-def fetch_album_list(limit=1, output='my_spotify_albums.json'):
+def get_albums(limit=50, output='my_spotify_albums.json'):
 
-    albums_url = SPOTIFY_API_URI + "me/albums"
     initial_call = True
-    total = 999
+    total = 9999999
     offset = 0
     albums = []
     resp = ""
 
     while offset < total:
         try:
-            response = requests.get(albums_url,
+            response = requests.get(ALBUM_URI,
                                     headers=GET_HEADERS,
                                     params={'limit': limit, 'offset': offset})
             resp = response.json()
 
             if "error" in resp:
-                print(resp)
                 break
 
             albums.extend(resp['items'])
@@ -51,14 +50,13 @@ def fetch_album_list(limit=1, output='my_spotify_albums.json'):
             print("oh crap, get request failed, don't ask why")
 
     if "error" not in resp:
-        out = {'items': albums}
-        fp = open(output, 'w')
-        json.dump(out, fp)
-        fp.close()
-        # return out
+        album_ids = [item['album']['id'] for item in albums]
+        return album_ids
+    else:
+        print(resp)
 
 
-def fetch_artist_list():
+def get_artists():
     pass
 
 
@@ -70,13 +68,39 @@ def show_artists():
     pass
 
 
-def put_artist_list():
+def put_artists():
     pass
 
 
-def put_album_list():
-    pass
+def post_albums(album_ids):
+
+    length = len(album_ids)
+    limit = 49
+    resp = ""
+
+    for i in range(0, length, limit):
+        # print(json.dumps(album_ids[i:i + limit]))
+        try:
+            response = requests.put(
+                ALBUM_URI,
+                headers=PUT_HEADERS,
+                # params={"ids": json.dumps(album_ids[i:i + limit])}
+
+                data=json.dumps(album_ids[i:i + limit])
+            )
+
+            print(response.text)
+
+        except requests.RequestException:
+            print("oh crap, get request failed, don't ask why ", resp)
+
+        time.sleep(1)
 
 
 if __name__ == "__main__":
-    fetch_album_list()
+    album_list = get_albums()
+
+    if album_list:
+        post_albums(album_list)
+
+    # post_albums([])
